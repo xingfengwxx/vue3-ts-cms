7357b656fdd8be1e5edc9f3c36643e4b28e39391
WangXingxing <1099420259@qq.com>
2022-10-24 16:19:27 +0800

    feat(test): test

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,27 @@
+<!--
+ * @Author: 王星星
+ * @Email: 1099420259@qq.com
+ * @Date: 2022-10-24 15:41:23
+ * @LastEditors: 王星星
+ * @LastEditTime: 2022-10-24 15:50:03
+ * @FilePath: \vue3-ts-cms\src\App.vue
+ * @Description:
+-->
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
   <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
-import HelloWorld from "./components/HelloWorld.vue";
+import { defineComponent } from 'vue'
+import HelloWorld from './components/HelloWorld.vue'
 
 export default defineComponent({
-  name: "App",
+  name: 'App',
   components: {
-    HelloWorld,
-  },
-});
+    HelloWorld
+  }
+})
 </script>
 
 <style lang="less">
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,13 @@
-import { createApp } from "vue";
-import App from "./App.vue";
+/*
+ * @Author: 王星星
+ * @Email: 1099420259@qq.com
+ * @Date: 2022-10-24 15:41:23
+ * @LastEditors: 王星星
+ * @LastEditTime: 2022-10-24 15:50:23
+ * @FilePath: \vue3-ts-cms\src\main.ts
+ * @Description:
+ */
+import { createApp } from 'vue'
+import App from './App.vue'
 
-createApp(App).mount("#app");
+createApp(App).mount('#app')
