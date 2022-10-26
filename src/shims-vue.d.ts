677298ce5ab16eae102792302870d39f781dace7
WangXingxing <1099420259@qq.com>
2022-10-26 10:08:28 +0800

    feat(login): 添加登录页面

--- src/shims-vue.d.ts
+++ src/shims-vue.d.ts
@@ -1,6 +1,17 @@
+/*
+ * @Author: 王星星
+ * @Email: 1099420259@qq.com
+ * @Date: 2022-10-24 15:41:23
+ * @LastEditors: 王星星
+ * @LastEditTime: 2022-10-26 09:34:19
+ * @FilePath: \vue3-ts-cms\src\shims-vue.d.ts
+ * @Description:
+ */
 /* eslint-disable */
 declare module '*.vue' {
   import type { DefineComponent } from 'vue'
   const component: DefineComponent<{}, {}, any>
   export default component
 }
+
+declare let $store: any
