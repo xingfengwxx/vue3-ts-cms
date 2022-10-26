14f40d2d60c37ae27690633fd034ee4a175eebd2
WangXingxing <1099420259@qq.com>
2022-10-26 10:21:29 +0800

    feat(login): 添加登录页面

--- src/App.vue
+++ src/App.vue
@@ -3,23 +3,12 @@
  * @Email: 1099420259@qq.com
  * @Date: 2022-10-24 15:41:23
  * @LastEditors: 王星星
- * @LastEditTime: 2022-10-25 11:53:33
+ * @LastEditTime: 2022-10-26 10:19:11
  * @FilePath: \vue3-ts-cms\src\App.vue
  * @Description:
 -->
 <template>
   <div class="app">
-    <h2>{{ $store.state.name }}</h2>
-    <router-link to="/login">登录</router-link>
-    <router-link to="/main">首页</router-link>
-
-    <el-button>默认按钮</el-button>
-    <el-button type="primary">主要按钮</el-button>
-    <el-button type="success">成功按钮</el-button>
-    <el-button type="info">信息按钮</el-button>
-    <el-button type="warning">警告按钮</el-button>
-    <el-button type="danger">危险按钮</el-button>
-
     <router-view></router-view>
   </div>
 </template>
